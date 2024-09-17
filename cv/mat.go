327f7cc3781d5a932b8c89f3ca71cc3372a20be4
deadprogram <ron@hybridgroup.com>
2024-09-17 16:54:54 +0200

    cv: add Mat.Close() and Mat.Reshape() methods

--- cv/mat.go
+++ cv/mat.go
@@ -10,11 +10,13 @@ import (
 func MatModules(cache *frame.Cache) wypes.Modules {
 	return wypes.Modules{
 		"wasm:cv/mat": wypes.Module{
-			"[constructor]mat":  wypes.H3(matNewFunc(cache)),
-			"[method]mat.cols":  wypes.H1(matColsFunc(cache)),
-			"[method]mat.rows":  wypes.H1(matRowsFunc(cache)),
-			"[method]mat.type":  wypes.H1(matTypeFunc(cache)),
-			"[method]mat.empty": wypes.H1(matEmptyFunc(cache)),
+			"[constructor]mat":    wypes.H3(matNewFunc(cache)),
+			"[method]mat.close":   wypes.H1(matCloseFunc(cache)),
+			"[method]mat.cols":    wypes.H1(matColsFunc(cache)),
+			"[method]mat.rows":    wypes.H1(matRowsFunc(cache)),
+			"[method]mat.type":    wypes.H1(matTypeFunc(cache)),
+			"[method]mat.empty":   wypes.H1(matEmptyFunc(cache)),
+			"[method]mat.reshape": wypes.H3(matReshapeFunc(cache)),
 		},
 	}
 }
@@ -29,6 +31,19 @@ func matNewFunc(cache *frame.Cache) func(wypes.UInt32, wypes.UInt32, wypes.UInt3
 		cache.Set(f)
 
 		return f.ID
+	}
+}
+
+func matCloseFunc(cache *frame.Cache) func(wypes.UInt32) wypes.Void {
+	return func(ref wypes.UInt32) wypes.Void {
+		f, ok := cache.Get(ref)
+		if !ok {
+			return wypes.Void{}
+		}
+		mat := f.Image
+		mat.Close()
+
+		return wypes.Void{}
 	}
 }
 
@@ -77,5 +92,23 @@ func matEmptyFunc(cache *frame.Cache) func(wypes.UInt32) wypes.Bool {
 		mat := f.Image
 
 		return wypes.Bool(mat.Empty())
+	}
+}
+
+func matReshapeFunc(cache *frame.Cache) func(wypes.UInt32, wypes.UInt32, wypes.UInt32) wypes.UInt32 {
+	return func(ref wypes.UInt32, channels wypes.UInt32, rows wypes.UInt32) wypes.UInt32 {
+		f, ok := cache.Get(ref)
+		if !ok {
+			return wypes.UInt32(0)
+		}
+		mat := f.Image
+
+		d := mat.Reshape(int(channels), int(rows))
+		dst := frame.NewFrame()
+		dst.SetImage(d)
+
+		cache.Set(dst)
+
+		return dst.ID
 	}
 }
