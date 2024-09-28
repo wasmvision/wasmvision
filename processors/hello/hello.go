42a62c9b2bc1f8bd6f6ce8d514de843674256dfa
deadprogram <ron@hybridgroup.com>
2024-09-28 09:56:11 +0200

    processors: update hello processor for return data
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- processors/hello/hello.go
+++ processors/hello/hello.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"unsafe"
+
 	"github.com/hybridgroup/mechanoid/convert"
 	"wasmcv.org/wasm/cv/mat"
 )
@@ -17,9 +19,21 @@ func process(image mat.Mat) mat.Mat {
 		" Rows: " +
 		convert.IntToString(int(image.Rows())) +
 		" Type: " +
-		convert.IntToString(int(image.Mattype()))))
+		convert.IntToString(int(image.Mattype())) +
+		" Size: " +
+		convert.IntToString(int(image.Size().Len()))))
 
 	return image
+}
+
+// malloc is needed for wasm-unknown-unknown target for functions that return a List.
+//
+//export malloc
+func malloc(size uint32) uint32 {
+	data := make([]byte, size)
+	ptr := uintptr(unsafe.Pointer(unsafe.SliceData(data)))
+
+	return uint32(ptr)
 }
 
 func main() {}
