05f686a43349a676bc9af38d3016b3871c8f86c5
deadprogram <ron@hybridgroup.com>
2024-09-29 19:50:25 +0200

    net: improve cache and also handle model file locations for loading
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- net/cache.go
+++ net/cache.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"errors"
+	"path/filepath"
 
 	"github.com/orsinium-labs/wypes"
 )
@@ -9,6 +10,8 @@ import (
 // Cache is a cache for Net.
 type Cache struct {
 	netCache map[wypes.UInt32]Net
+
+	ModelsDir string
 }
 
 // NewCache creates a new Cache.
@@ -20,8 +23,8 @@ func NewCache() *Cache {
 
 // Get returns a Net from the cache.
 func (c *Cache) Get(id wypes.UInt32) (Net, bool) {
-	frame, ok := c.netCache[id]
-	return frame, ok
+	net, ok := c.netCache[id]
+	return net, ok
 }
 
 // Set sets a Net in the cache.
@@ -39,9 +42,15 @@ func (c *Cache) Delete(id wypes.UInt32) {
 	delete(c.netCache, id)
 }
 
-// Close closes all Net in the cache.
+// Close closes all Net in the cache and deletes them.
 func (c *Cache) Close() {
 	for _, n := range c.netCache {
 		n.Close()
+		c.Delete(n.ID)
 	}
+}
+
+// ModelFile gets the model file path name for the Net.
+func (c *Cache) ModelFileName(model string) string {
+	return filepath.Join(c.ModelsDir, model)
 }
