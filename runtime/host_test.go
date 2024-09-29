6356f9fece19c8517688086ff26459abe2486b4e
deadprogram <ron@hybridgroup.com>
2024-09-29 19:52:13 +0200

    runtime: add InterpreterConfig for passing runtime config settings
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- runtime/host_test.go
+++ runtime/host_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNewInterpreter(t *testing.T) {
-	interpreter := New(context.Background())
+	interpreter := New(context.Background(), InterpreterConfig{})
 	if len(interpreter.Processors()) != 0 {
 		t.Error("interpreter is invalid")
 	}
