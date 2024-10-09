c062dc25da1feee63694a16d7a7bd35763dd78eb
deadprogram <ron@hybridgroup.com>
2024-10-09 11:05:36 +0200

    processors: switch order of params back to match original now that Size, RGBA, and Scalar types exist
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- processors/facedetectyn/facedetect.go
+++ processors/facedetectyn/facedetect.go
@@ -44,36 +44,36 @@ func process(image mat.Mat) mat.Mat {
 	out := image.Clone()
 
 	for r := uint32(0); r < faces.Rows(); r++ {
-		x0 := int32(faces.GetFloatAt(r, 1))
-		y0 := int32(faces.GetFloatAt(r, 0))
-		x1 := x0 + int32(faces.GetFloatAt(r, 3))
-		y1 := y0 + int32(faces.GetFloatAt(r, 2))
+		x0 := int32(faces.GetFloatAt(r, 0))
+		y0 := int32(faces.GetFloatAt(r, 1))
+		x1 := x0 + int32(faces.GetFloatAt(r, 2))
+		y1 := y0 + int32(faces.GetFloatAt(r, 3))
 
 		faceRect := types.Rect{Min: types.Size{X: x0, Y: y0}, Max: types.Size{X: x1, Y: y1}}
 
 		rightEye := types.Size{
-			X: int32(faces.GetFloatAt(r, 5)),
-			Y: int32(faces.GetFloatAt(r, 4)),
+			X: int32(faces.GetFloatAt(r, 4)),
+			Y: int32(faces.GetFloatAt(r, 5)),
 		}
 
 		leftEye := types.Size{
-			X: int32(faces.GetFloatAt(r, 7)),
-			Y: int32(faces.GetFloatAt(r, 6)),
+			X: int32(faces.GetFloatAt(r, 6)),
+			Y: int32(faces.GetFloatAt(r, 7)),
 		}
 
 		noseTip := types.Size{
-			X: int32(faces.GetFloatAt(r, 9)),
-			Y: int32(faces.GetFloatAt(r, 8)),
+			X: int32(faces.GetFloatAt(r, 8)),
+			Y: int32(faces.GetFloatAt(r, 9)),
 		}
 
 		rightMouthCorner := types.Size{
-			X: int32(faces.GetFloatAt(r, 11)),
-			Y: int32(faces.GetFloatAt(r, 10)),
+			X: int32(faces.GetFloatAt(r, 10)),
+			Y: int32(faces.GetFloatAt(r, 11)),
 		}
 
 		leftMouthCorner := types.Size{
-			X: int32(faces.GetFloatAt(r, 13)),
-			Y: int32(faces.GetFloatAt(r, 12)),
+			X: int32(faces.GetFloatAt(r, 12)),
+			Y: int32(faces.GetFloatAt(r, 13)),
 		}
 
 		cv.Rectangle(out, faceRect, green, 1)
