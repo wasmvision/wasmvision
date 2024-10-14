6a00bb10bb800e6905d485c844313fd69bdcc095
deadprogram <ron@hybridgroup.com>
2024-10-14 20:12:48 +0200

    runtime: implement host functions for wasmvision:platform/http interface
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- runtime/hostfuncs.go
+++ runtime/hostfuncs.go
@@ -1,7 +1,12 @@
 package runtime
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/orsinium-labs/wypes"
 	"github.com/wasmvision/wasmvision/cv"
@@ -9,18 +14,25 @@ import (
 
 func hostedModules(ctx *cv.Context) wypes.Modules {
 	return wypes.Modules{
+		"wasmvision:platform/config": wypes.Module{
+			"get-config": wypes.H3(hostGetConfigFunc(ctx)),
+		},
+		"wasmvision:platform/http": wypes.Module{
+			"get":  wypes.H3(httpGetFunc(ctx)),
+			"post": wypes.H5(httpPostFunc(ctx)),
+		},
 		"wasmvision:platform/logging": wypes.Module{
 			"println": wypes.H1(hostPrintln),
 			"log":     wypes.H1(hostLogFunc(ctx)),
 		},
-		"wasmvision:platform/config": wypes.Module{
-			"get-config": wypes.H3(hostGetConfig(ctx)),
+		"wasmvision:platform/time": wypes.Module{
+			"now": wypes.H2(hostTimeNowFunc(ctx)),
 		},
 	}
 }
 
 func hostPrintln(msg wypes.String) wypes.Void {
-	println(msg.Unwrap())
+	fmt.Println(msg.Unwrap())
 	return wypes.Void{}
 }
 
@@ -33,7 +45,7 @@ func hostLogFunc(ctx *cv.Context) func(wypes.String) wypes.Void {
 	}
 }
 
-func hostGetConfig(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.Result[wypes.String, wypes.String, wypes.UInt32]) wypes.Void {
+func hostGetConfigFunc(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.Result[wypes.String, wypes.String, wypes.UInt32]) wypes.Void {
 	return func(s *wypes.Store, key wypes.String, result wypes.Result[wypes.String, wypes.String, wypes.UInt32]) wypes.Void {
 		if s.Error != nil {
 			log.Printf("error in store after lift: %v\n", s.Error)
@@ -55,5 +67,95 @@ func hostGetConfig(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.Resul
 		}
 
 		return wypes.Void{}
+	}
+}
+
+func httpGetFunc(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+	return func(s *wypes.Store, url wypes.String, result wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+		if ctx.Logging {
+			log.Println("http get:", url.Unwrap())
+		}
+
+		resp, err := http.Get(url.Unwrap())
+		if err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		// HACK: limit the size of the response to 128 bytes, for now.
+		max := 128
+		if len(body) < max {
+			max = len(body)
+		}
+
+		result.IsError = false
+		result.OK = wypes.Bytes{Raw: body[:max]}
+		result.DataPtr = ctx.ReturnDataPtr
+
+		result.Lower(s)
+		if s.Error != nil {
+			log.Printf("httpGetFunc error in store after lower: %v\n", s.Error)
+		}
+
+		return wypes.Void{}
+	}
+}
+
+func httpPostFunc(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.String, wypes.Bytes, wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+	return func(s *wypes.Store, url wypes.String, contentType wypes.String, request wypes.Bytes, result wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+		if ctx.Logging {
+			log.Println("http post:", url.Unwrap())
+		}
+
+		reqBody := bytes.NewBuffer(request.Raw)
+
+		resp, err := http.Post(url.Unwrap(), contentType.Unwrap(), reqBody)
+		if err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		// HACK: limit the size of the response to 128 bytes, for now.
+		max := 128
+		if len(body) < max {
+			max = len(body)
+		}
+
+		result.IsError = false
+		result.OK = wypes.Bytes{Raw: body[:max]}
+		result.DataPtr = ctx.ReturnDataPtr
+
+		result.Lower(s)
+		if s.Error != nil {
+			log.Printf("httpGetFunc error in store after lower: %v\n", s.Error)
+		}
+
+		return wypes.Void{}
+	}
+}
+
+func hostTimeNowFunc(ctx *cv.Context) func(*wypes.Store, wypes.UInt32) wypes.UInt64 {
+	return func(*wypes.Store, wypes.UInt32) wypes.UInt64 {
+		t := time.Now().UnixMicro()
+
+		return wypes.UInt64(t)
 	}
 }
