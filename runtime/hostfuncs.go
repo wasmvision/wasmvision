90f11da7536516f500318e4096310275b832662f
deadprogram <ron@hybridgroup.com>
2024-10-14 23:27:59 +0200

    runtime: add support for http PostImage function
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- runtime/hostfuncs.go
+++ runtime/hostfuncs.go
@@ -2,14 +2,18 @@ package runtime
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/orsinium-labs/wypes"
 	"github.com/wasmvision/wasmvision/cv"
+	"gocv.io/x/gocv"
 )
 
 func hostedModules(ctx *cv.Context) wypes.Modules {
@@ -18,8 +22,9 @@ func hostedModules(ctx *cv.Context) wypes.Modules {
 			"get-config": wypes.H3(hostGetConfigFunc(ctx)),
 		},
 		"wasmvision:platform/http": wypes.Module{
-			"get":  wypes.H3(httpGetFunc(ctx)),
-			"post": wypes.H5(httpPostFunc(ctx)),
+			"get":        wypes.H3(httpGetFunc(ctx)),
+			"post":       wypes.H5(httpPostFunc(ctx)),
+			"post-image": wypes.H7(httpPostImageFunc(ctx)),
 		},
 		"wasmvision:platform/logging": wypes.Module{
 			"println": wypes.H1(hostPrintln),
@@ -145,7 +150,70 @@ func httpPostFunc(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.String
 
 		result.Lower(s)
 		if s.Error != nil {
-			log.Printf("httpGetFunc error in store after lower: %v\n", s.Error)
+			log.Printf("httpPostFunc error in store after lower: %v\n", s.Error)
+		}
+
+		return wypes.Void{}
+	}
+}
+
+func httpPostImageFunc(ctx *cv.Context) func(*wypes.Store, wypes.String, wypes.String, wypes.Bytes, wypes.String, wypes.HostRef[*cv.Frame], wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+	return func(s *wypes.Store, url wypes.String, contentType wypes.String, template wypes.Bytes, responseKey wypes.String, mat wypes.HostRef[*cv.Frame], result wypes.Result[wypes.Bytes, wypes.Bytes, wypes.UInt32]) wypes.Void {
+		if ctx.Logging {
+			log.Println("http post image:", url.Unwrap())
+		}
+
+		// TODO: support other content types
+		ct := "application/json"
+
+		buffer, err := gocv.IMEncode(gocv.JPEGFileExt, mat.Raw.Image)
+		if err != nil {
+			log.Printf("error encoding image: %v\n", err)
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		sEnc := base64.StdEncoding.EncodeToString(buffer.GetBytes())
+
+		tmpl := string(template.Raw)
+		tmpl = strings.Replace(tmpl, "%IMAGE%", sEnc, -1)
+
+		reqBody := bytes.NewBuffer([]byte(tmpl))
+
+		resp, err := http.Post(url.Unwrap(), ct, reqBody)
+		if err != nil {
+			log.Printf("error posting image: %v\n", err)
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+
+		var payload interface{}
+		if err := json.Unmarshal(body, &payload); err != nil {
+			result.IsError = true
+			result.Error = wypes.UInt32(3) // HTTPErrorRequestError
+			return wypes.Void{}
+		}
+		m := payload.(map[string]interface{})
+
+		val := m[responseKey.Unwrap()].(string)
+
+		result.IsError = false
+		result.OK = wypes.Bytes{Raw: []byte(val)}
+		result.DataPtr = ctx.ReturnDataPtr
+
+		result.Lower(s)
+		if s.Error != nil {
+			log.Printf("httpPostImageFunc error in store after lower: %v\n", s.Error)
 		}
 
 		return wypes.Void{}
