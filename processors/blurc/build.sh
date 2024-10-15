cf64f0d05eeff4e99fc5124261dbe530dcf536e1
deadprogram <ron@hybridgroup.com>
2024-10-15 19:53:32 +0200

    processors: modify C processor to use wasmvision:platform/logging
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- processors/blurc/build.sh
+++ processors/blurc/build.sh
@@ -6,11 +6,16 @@ WASI_LIBC_SYSROOT=$(tinygo env TINYGOROOT)
 # must change this to match your wasmcv C files installation
 WASMCV_C_COMPONENTS_PATH="../../../wasmcv/components/c"
 
+# must change this to match your wasmcv C files installation
+WASMVISION_C_COMPONENTS_PATH="../../../wasmvision-sdk/components/c"
+
 /opt/wasi-sdk/bin/clang --target=wasm32-unknown-unknown -O3 \
         --sysroot="${WASI_LIBC_SYSROOT}/lib/wasi-libc/sysroot" \
         -z stack-size=4096 -Wl,--initial-memory=65536 \
-        -I$WASMCV_C_COMPONENTS_PATH \
-        -o ../blurc.wasm process.c $WASMCV_C_COMPONENTS_PATH/wasmcv/imports.c $WASMCV_C_COMPONENTS_PATH/wasmcv/imports_component_type.o \
+        -I$WASMCV_C_COMPONENTS_PATH -I$WASMVISION_C_COMPONENTS_PATH \
+        -o ../blurc.wasm process.c \
+        $WASMCV_C_COMPONENTS_PATH/wasmcv/imports.c $WASMCV_C_COMPONENTS_PATH/wasmcv/imports_component_type.o \
+        $WASMVISION_C_COMPONENTS_PATH/wasmvision/platform.c $WASMVISION_C_COMPONENTS_PATH/wasmvision/platform_component_type.o \
         -Wl,--export=process \
         -Wl,--export=__data_end -Wl,--export=__heap_base \
         -Wl,--strip-all,--no-entry \
