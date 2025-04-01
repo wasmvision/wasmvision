51a51d3be92df4305d850278040959acc73987d4
deadprogram <ron@hybridgroup.com>
2025-04-01 18:09:31 +0200

    processors: improvements to captions.wasm for configuration and memory correctness
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- processors/captions/README.md
+++ processors/captions/README.md
@@ -15,3 +15,47 @@ wasmvision run -p ollama.wasm -p captions.wasm
 ```
 
 The `ollama.wasm` processor uses the built-in datastore to save any information about the scene description. The `captions.wasm` processor looks for this information and then displays this description on the output image.
+
+## Configuration
+
+The following configuration settings are available for the `captions.wasm` processor.
+
+### `caption-words-per-line`
+
+Set the number of words per line for the captioning like this:
+
+```shell
+-c caption-words-per-line=7
+```
+
+Default value: 10
+
+### `caption-line-height`
+
+Set the line height for the captioning like this:
+
+```shell
+-c caption-line-height=30
+```
+
+Default value: 20
+
+### `caption-size`
+
+Set the font size for the captioning like this:
+
+```shell
+-c caption-size=0.8
+```
+
+Default value: 0.6
+
+### `caption-color`
+
+Set the font color for the captioning like this:
+
+```shell
+-c caption-color=red
+```
+
+Default value: "black"
---
+++ processors/captions/config.go
@@ -0,0 +1,104 @@
+//go:build tinygo
+
+package main
+
+import (
+	"strconv"
+
+	"github.com/wasmvision/wasmvision-sdk-go/config"
+	"github.com/wasmvision/wasmvision-sdk-go/logging"
+	"wasmcv.org/wasm/cv/types"
+)
+
+const (
+	captionWordsPerLineDefault = 10
+	captionSizeDefault         = 0.6
+	captionLineHeightDefault   = 20
+)
+
+var (
+	captionWordsPerLine int64
+	captionLineHeight   int64
+	captionSize         float64
+	captionColorName    string
+	captionColor        types.RGBA
+
+	captionColorDefault = captionColorBlack
+	captionColorBlack   = types.RGBA{R: 0, G: 0, B: 0, A: 0}
+	captionColorWhite   = types.RGBA{R: 255, G: 255, B: 255, A: 0}
+	captionColorRed     = types.RGBA{R: 255, G: 0, B: 0, A: 0}
+	captionColorGreen   = types.RGBA{R: 0, G: 255, B: 0, A: 0}
+	captionColorBlue    = types.RGBA{R: 0, G: 0, B: 255, A: 0}
+	captionColorYellow  = types.RGBA{R: 255, G: 255, B: 0, A: 0}
+)
+
+// loadConfig loads the configuration for caption size and color from the config store.
+// If the configuration is not set, it uses default values.
+func loadConfig() {
+	if captionWordsPerLine == 0 {
+		ok, _, isErr := config.GetConfig("caption-words-per-line").Result()
+		if isErr {
+			captionWordsPerLine = captionWordsPerLineDefault
+		} else {
+			captionWordsPerLine, _ = strconv.ParseInt(ok, 10, 64)
+		}
+		logging.Info("Using caption words per line " + strconv.Itoa(int(captionWordsPerLine)))
+	}
+
+	if captionLineHeight == 0 {
+		ok, _, isErr := config.GetConfig("caption-line-height").Result()
+		if isErr {
+			captionLineHeight = captionLineHeightDefault
+		} else {
+			var err error
+			captionLineHeight, err = strconv.ParseInt(ok, 10, 64)
+			if err != nil {
+				logging.Error("Error parsing line height: " + err.Error())
+				captionLineHeight = captionLineHeightDefault
+			}
+		}
+
+		logging.Info("Using caption line height " + strconv.Itoa(int(captionLineHeight)))
+	}
+
+	if captionSize == 0 {
+		ok, _, isErr := config.GetConfig("caption-size").Result()
+		if isErr {
+			captionSize = captionSizeDefault
+		} else {
+			captionSize, _ = strconv.ParseFloat(ok, 64)
+		}
+
+		logging.Info("Using caption size " + strconv.FormatFloat(captionSize, 'f', -1, 64))
+	}
+
+	if captionColorName == "" {
+		ok, _, isErr := config.GetConfig("caption-color").Result()
+		if isErr {
+			captionColorName = "black"
+			captionColor = captionColorBlack
+		} else {
+			captionColorName = ok
+			switch captionColorName {
+			case "black":
+				captionColor = captionColorBlack
+			case "white":
+				captionColor = captionColorWhite
+			case "red":
+				captionColor = captionColorRed
+			case "green":
+				captionColor = captionColorGreen
+			case "blue":
+				captionColor = captionColorBlue
+			case "yellow":
+				captionColor = captionColorYellow
+			default:
+				captionColorName = "black"
+				// default to black if the color is not recognized
+				captionColor = captionColorBlack
+			}
+		}
+
+		logging.Info("Using color " + captionColorName)
+	}
+}
