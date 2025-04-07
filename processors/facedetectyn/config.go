aff92511b886b06a5ab2b25fc08fdc3cf1326605
deadprogram <ron@hybridgroup.com>
2025-04-07 10:24:13 +0200

    processors: add config setting to facedetectyn to turn on/off drawing of face rects
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ processors/facedetectyn/config.go
@@ -0,0 +1,24 @@
+//go:build tinygo
+
+package main
+
+import (
+	"github.com/wasmvision/wasmvision-sdk-go/config"
+)
+
+var (
+	checkedDrawFaceBoxes bool
+	drawFaceBoxes        bool
+)
+
+func loadConfig() {
+	if !checkedDrawFaceBoxes {
+		drawFaceBoxes = true
+		checkedDrawFaceBoxes = true
+
+		ok, _, isErr := config.GetConfig("draw-faces").Result()
+		if !isErr && ok == "false" {
+			drawFaceBoxes = false
+		}
+	}
+}
